7a0de7b9ce59b0ef518c6f27f4b887e61217d279
CesarFT0 <tobby.ftp@gmail.com>
2023-09-13 12:28:22 -0500

    first commit

---
+++ quizapp/src/Components/QuestionBank.js
@@ -0,0 +1,40 @@
+const qBank = [
+	{
+		id: 1,
+		question: "Which function is used to serialize an object into JSON string in Javascript?",
+		options: ["stringify()", "parse()", "convert()", "None of the above"],
+		answer: "stringify()",
+	},
+	{
+		id: 2,
+		question: "Which of the following keywords is used to define a variable in Javascript?",
+		options: ["var", "let", "var and let", "None of the above"],
+		answer: "var and let",
+	},
+	{
+		id: 3,
+		question: "Which of the following methods can be used to display data in some form using Javascript?",
+		options: ["document.write()", "console.log()", "window.alert", "All of the above"],
+		answer: "All of the above"
+	},
+	{
+		id: 4,
+		question: "How can a datatype be declared to be a constant type?",
+		options: ["const", "var", "let", "constant"],
+		answer: "const"
+	},
+	{
+		id: 5,
+		question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+		options: ["Throws an error", "Ignores the statements", "Gives a warning", "None of the above"],
+		answer: "Ignores the statements"
+	},
+    {
+		id: 6,
+		question: "What keyword is used to check whether a given property is valid or not?",
+		options: ["in", "is in", "exists", "lies"],
+		answer: "in"
+	},
+]
+
+export default qBank;
