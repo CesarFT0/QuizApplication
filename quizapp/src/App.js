35303717028e82d79d9f074a7c7adfad821cf8e9
CesarFT0 <tobby.ftp@gmail.com>
2023-09-19 13:46:14 -0500

    Frontend integration with backend - Display Quiz topics and Questions based on topic selected

--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -1,76 +1,129 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Question from "./Components/Question";
-import qBank from "./Components/QuestionBank";
 import Score from "./Components/Score";
+import TopicSelector from "./Components/TopicSelector";
 import "./App.css";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			questionBank: qBank,
-			currentQuestion: 0,
-			selectedOption: "",
-			score: 0,
-			quizEnd: false,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionBank: [],
+      currentQuestion: 0,
+      selectedOption: "",
+      score: 0,
+      quizEnd: false,
+      selectedTopic: null,
+    };
+  }
 
-	handleOptionChange = (e) => {
-		this.setState({ selectedOption: e.target.value });
-	};
+  componentDidMount() {
+    
+    fetch("/questions/getQuestionsByTopic/1")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ questionBank: data });
+      })
+      .catch((error) => {
+        console.error("Error fetching questions:", error);
+      });
+  }
 
-	handleFormSubmit = (e) => {
-		e.preventDefault();
-		this.checkAnswer();
-		this.handleNextQuestion();
-	};
+  handleOptionChange = (e) => {
+    this.setState({ selectedOption: e.target.value });
+  };
 
-	checkAnswer = () => {
-		const { questionBank, currentQuestion, selectedOption, score } = this.state;
-		if (selectedOption === questionBank[currentQuestion].answer) {
-			this.setState((prevState) => ({ score: prevState.score + 1 }));
-		}
-	};
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.checkAnswer();
+    this.handleNextQuestion();
+  };
 
-	handleNextQuestion = () => {
-		const { questionBank, currentQuestion } = this.state;
-		if (currentQuestion + 1 < questionBank.length) {
-			this.setState((prevState) => ({
-				currentQuestion: prevState.currentQuestion + 1,
-				selectedOption: "",
-			}));
-		} else {
-			this.setState({
-				quizEnd: true,
-			});
-		}
-	};
+  checkAnswer = () => {
+    const { questionBank, currentQuestion, selectedOption, score } = this.state;
+    const currentQuestionData = questionBank[currentQuestion];
 
-	render() {
-		const { questionBank, currentQuestion, selectedOption, score, quizEnd } =
-			this.state;
-		return (
-			<div className="App d-flex flex-column align-items-center justify-content-center">
-				<h1 className="app-title">QUIZ APP</h1>
-				{!quizEnd ? (
-					<Question
-						question={questionBank[currentQuestion]}
-						selectedOption={selectedOption}
-						onOptionChange={this.handleOptionChange}
-						onSubmit={this.handleFormSubmit}
-					/>
-				) : (
-					<Score
-						score={score}
-						onNextQuestion={this.handleNextQuestion}
-						className="score"
-					/>
-				)}
-			</div>
-		);
-	}
+    if (currentQuestionData && selectedOption === currentQuestionData.correctAnswer) {
+      this.setState((prevState) => ({ score: prevState.score + 1 }));
+    }
+  };
+
+  handleNextQuestion = () => {
+    const { questionBank, currentQuestion } = this.state;
+    if (currentQuestion + 1 < questionBank.length) {
+      this.setState((prevState) => ({
+        currentQuestion: prevState.currentQuestion + 1,
+        selectedOption: "",
+      }));
+    } else {
+      this.setState({
+        quizEnd: true,
+      });
+    }
+  };
+
+  handleStartQuiz = (topicId) => {
+    
+    fetch(`/questions/getQuestionsByTopic/${topicId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          questionBank: data,
+          selectedTopic: topicId,
+          currentQuestion: 0,
+          selectedOption: "",
+          score: 0,
+          quizEnd: false,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching questions:", error);
+      });
+  };
+
+  render() {
+    const {
+      questionBank,
+      currentQuestion,
+      selectedOption,
+      score,
+      quizEnd,
+      selectedTopic,
+    } = this.state;
+    const currentQuestionData = questionBank[currentQuestion];
+
+    return (
+      <div className="App d-flex flex-column align-items-center justify-content-center">
+        <h1 className="app-title">QUIZ APP</h1>
+        {!selectedTopic ? (
+          <TopicSelector onStartQuiz={this.handleStartQuiz} />
+        ) : (
+          !quizEnd && currentQuestionData ? (
+            <Question
+              question={{
+                id: currentQuestion + 1, 
+                question: currentQuestionData.questionDescription, 
+                options: [
+                  currentQuestionData.option1,
+                  currentQuestionData.option2,
+                  currentQuestionData.option3,
+                  currentQuestionData.option4,
+                ],
+                answer: currentQuestionData.correctAnswer,
+              }}
+              selectedOption={selectedOption}
+              onOptionChange={this.handleOptionChange}
+              onSubmit={this.handleFormSubmit}
+              firstQuestionId={0} 
+            />
+          ) : (
+            <Score score={score} onNextQuestion={this.handleNextQuestion} />
+          )
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ quizapp/src/Components/TopicSelector.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+class TopicSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topics: [],
+      selectedTopic: "",
+    };
+  }
+
+  componentDidMount() {
+    
+    fetch("/topic/getAllTopics")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ topics: data });
+      })
+      .catch((error) => {
+        console.error("Error fetching topics:", error);
+      });
+  }
+
+  handleTopicChange = (e) => {
+    this.setState({ selectedTopic: e.target.value });
+  };
+
+  handleStartQuiz = () => {
+    
+    this.props.onStartQuiz(this.state.selectedTopic);
+  };
+
+  render() {
+    const { topics, selectedTopic } = this.state;
+
+    return (
+      <div>
+        <h1>Select a Topic</h1>
+        <div className="form-group">
+          <select
+            className="form-control"
+            value={selectedTopic}
+            onChange={this.handleTopicChange}
+          >
+            <option value="">Select a Topic</option>
+            {topics.map((topic) => (
+              <option key={topic.topicId} value={topic.topicId}>
+                {topic.topicName}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button
+          className="btn btn-primary"
+          onClick={this.handleStartQuiz}
+          disabled={!selectedTopic}
+        >
+          Start Quiz
+        </button>
+      </div>
+    );
+  }
+}
+
+export default TopicSelector;
+
+
+
+
+
+  
+
